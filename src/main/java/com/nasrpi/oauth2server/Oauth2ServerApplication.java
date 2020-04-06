4da011f4ba5f405511d51e84999afd5fcfbf83d4
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-04-07 01:05:19 +0530

    created packages

--- src/main/java/com/nasrpi/oauth2server/Oauth2ServerApplication.java
+++ src/main/java/com/nasrpi/oauth2server/Oauth2ServerApplication.java
@@ -1,8 +1,14 @@
+/**
+ * Copyright nasrpi 2020
+ */
 package com.nasrpi.oauth2server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zuiee
+ */
 @SpringBootApplication
 public class Oauth2ServerApplication {
 
